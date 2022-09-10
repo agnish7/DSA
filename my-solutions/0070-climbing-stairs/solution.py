6702cea8c4671d87aede3f4a6ac7539fcb50f6c4
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2022-09-10 06:59:40 +0000

    Sync LeetCode submission Runtime - 31 ms (0.68%), Memory - 13.3 MB (19.77%)

---
+++ my-solutions/0070-climbing-stairs/solution.py
@@ -0,0 +1,22 @@
+from math import sqrt
+
+class Solution(object):
+    steps_map = {}
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        if n == 1:
+            return 1
+        dp = {}
+        
+        dp[1] = 1
+        dp[2] = 2
+        
+        dp[3] = 3
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+        
