f96a11a43af76e4d4299c133e9220e9dac099048
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2022-11-24 03:17:21 +0000

    Sync LeetCode submission Runtime - 233 ms (6.02%), Memory - 21.8 MB (100.00%)

--- my-solutions/0124-binary-tree-maximum-path-sum/solution.py
+++ my-solutions/0124-binary-tree-maximum-path-sum/solution.py
@@ -4,18 +4,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-import math
-
 class Solution:
     def maxPath(self, root):
-        if root == None:
-            return [-math.inf, -math.inf]
-        else:
-            left = self.maxPath(root.left)
-            right = self.maxPath(root.right)
-            wo = max(root.val + left[0] + right[0], left[1], right[1], left[0], right[0])
-            wm = max(root.val + left[0], root.val + right[0], root.val)
-            return [wm, wo]
-    
+        if not root:
+            return[float("-inf"), float("-inf")]
+        
+        if not root.left and not root.right:
+            return [root.val, root.val]
+
+        left = self.maxPath(root.left)
+        right = self.maxPath(root.right)
+
+        # include parent max(left include, right include) + root.val
+        res = [max(max(left[0], right[0]) + root.val, root.val)]
+        # not include parent max(left not include, right not include, root val, root val + left include + right include)
+        res.append(max(left[1], right[1], root.val, root.val + left[0] + right[0], root.val + left[0], root.val + right[0]))
+
+        return res
+
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         return max(self.maxPath(root))
