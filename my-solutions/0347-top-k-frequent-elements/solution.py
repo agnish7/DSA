97c0f3202e0457314a736e0a42c6efad3802bd22
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2022-12-23 04:00:04 +0000

    Sync LeetCode submission Runtime - 635 ms (5.14%), Memory - 23.5 MB (26.40%)

--- my-solutions/0347-top-k-frequent-elements/solution.py
+++ my-solutions/0347-top-k-frequent-elements/solution.py
@@ -1,32 +1,26 @@
-import math
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counts = [[] for i in range(10000)]
-        map_freq = {}
-        result = []
+        freq = [[] for i in range(100000)]
+        count = {}
         for i in range(len(nums)):
-            if nums[i] in map_freq:
-                map_freq[nums[i]] += 1
+            if nums[i] in count:
+                freq[count[nums[i]]].remove(nums[i])
+                freq[count[nums[i]] + 1].append(nums[i])
+                count[nums[i]] += 1
             else:
-                map_freq[nums[i]] = 1
+                freq[1].append(nums[i])
+                count[nums[i]] = 1
+        
+        res = []
+        i = len(freq)
+        while(k > 0):
+            i -= 1
+            if len(freq[i]) != 0:
+                for j in range(len(freq[i])):
+                    res.append(freq[i][j])
+                    k -= 1
+                    if k == 0:
+                        break
 
-        for key in map_freq.keys():
-            count = map_freq[key]
-            counts[count].append(key)
-            
-        # print(map_freq)
-        # print(counts)
-          
-        i = 1
-        while k > 0 and i < len(counts):
-            if len(counts[len(counts) - i]) != 0:
-                if k > len(counts[len(counts) - i]):
-                    result.extend(counts[len(counts) - i])
-                    k -= len(counts[len(counts) - i])
-                else:
-                    result.extend(counts[len(counts) - i][:k])
-                    return result
-            i += 1
-            
-        return result
-            
+        return res
+
