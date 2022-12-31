a2fcd7a0da4078ad4ff95c4d03651d7a2906686c
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2022-12-31 18:27:25 +0000

    Sync LeetCode submission Runtime - 36 ms (0.50%), Memory - 13.9 MB (100.00%)

--- my-solutions/0213-house-robber-ii/solution.py
+++ my-solutions/0213-house-robber-ii/solution.py
@@ -1,28 +1,24 @@
-class Solution(object):
-    def __init__(self):
-        self.cache = {}
-    def robber(self, nums, i , j):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
-        # recurrence relation: rob(1:n) = max(arr[0] + rob(2:n-1), rob(1:n))
-
-        if i > j:
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0:
             return 0
-        if i == j:
-            return nums[i]
-        if (i, j) in self.cache:
-            return self.cache[(i, j)]
-        else:
-            res = -1
-            if i == 0:
-                res = max(nums[i] + self.robber(nums, i+2, j - 1), self.robber(nums, i+1, j))
-            else:
-                res = max(nums[i] + self.robber(nums, i+2, j), self.robber(nums, i+1, j))
-            self.cache[(i,j)] = res
-            return res
+        if len(nums) == 1:
+            return nums[0]
 
-    def rob(self, nums):
-        return self.robber(nums, 0, len(nums) - 1)
+        dp1 = [0 for i in range(len(nums) + 1)]
+        dp2 = [0 for i in range(len(nums) + 1)]
+
+        dp1[len(nums) - 1] = 0
+        dp1[len(nums)] = 0
+
+        dp2[0] = 0
+        dp2[1] = 0
+
+        for i in range(len(nums) - 2, -1, -1):
+            dp1[i] = max(nums[i] + dp1[i+2], dp1[i+1])
+
+        for i in range(2, len(nums)+1):
+            dp2[i] = max(nums[i-1] + dp2[i-2], dp2[i-1])
+
+        return max(dp1[0], dp2[-1])
+
