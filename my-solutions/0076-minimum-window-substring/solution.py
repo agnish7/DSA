c3c2ea6a565057093e075c38a85bb9b4cff1083c
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-01-05 16:18:39 +0000

    Sync LeetCode submission Runtime - 653 ms (5.53%), Memory - 14.8 MB (100.00%)

--- my-solutions/0076-minimum-window-substring/solution.py
+++ my-solutions/0076-minimum-window-substring/solution.py
@@ -1,78 +1,27 @@
-import math
-
 class Solution:
-    def contains(self, dict1, dict2):
-        res = True
-        
-        for key in dict1.keys():
-            if key not in dict2:
-                return False
-            elif dict1[key] > dict2[key]:
-                return False
-            else:
-                continue
-        
-        return res
-        
     def minWindow(self, s: str, t: str) -> str:
-        
-        t_dict = {}
-        
-        for i in range(len(t)):
-            if t[i] in t_dict:
-                t_dict[t[i]] += 1
-            else:
-                t_dict[t[i]] = 1
-        
-        min_window = ""
-        min_len = math.inf
-        
-#         for i in range(len(s)):
-#             for j in range(i, len(s)):
-#                 s_dict = {}
-#                 for k in range(i, j+1):
-#                     if s[k] in s_dict:
-#                         s_dict[s[k]] += 1
-#                     else:
-#                         s_dict[s[k]] = 1
-                        
-#                 # print(i, j)
-#                 # print(t_dict, s_dict)
-#                 # print(self.contains(t_dict, s_dict), min_len,  j-i+1)
-#                 # print(s[i:j+1])
+        countT, window = {}, {}
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
 
-#                 if self.contains(t_dict, s_dict) and min_len > j-i+1:
-#                     min_window = s[i:j+1]
-#                     min_len = j-i+1
-                    
-#                 # print(min_len,  j-i+1)
-#                 # print(" ")
-        
-        s_dict = {}
-        last = 0
+        have, need = 0, len(countT)
+        j = 0
+        res, reslen = "", float("inf")
         for i in range(len(s)):
-            if s[i] in s_dict:
-                s_dict[s[i]] += 1
-            else:
-                s_dict[s[i]] = 1
-            
-            while(self.contains(t_dict, s_dict) and last < len(s)):
-                if s[last] in s_dict:
-                    if s_dict[s[last]] > 1:
-                        s_dict[s[last]] -= 1
-                    else:
-                        del s_dict[s[last]]
-                last += 1
-                
-                if i - last < min_len:
-                    min_len = i - last
-                    min_window = s[last-1:i+1]
-                
-                # print(min_window, i, last)
-            
-            
-            
-            
+            window[s[i]] = 1 + window.get(s[i], 0)
+            if s[i] in countT and countT[s[i]] == window[s[i]]:
+                have += 1
         
-        return min_window
-                        
+            
+            while have == need:
+                # print(window)
+                if i - j + 1 < reslen:
+                    res = s[j : i+1]
+                    # print(res)
+                    reslen = i - j + 1
+                window[s[j]] -= 1
+                if s[j] in countT and countT[s[j]] > window[s[j]]:
+                    have -= 1
+                j += 1
+
+        return res
