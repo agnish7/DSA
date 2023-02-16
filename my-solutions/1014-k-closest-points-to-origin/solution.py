78e900c932d21c12ef0eb16124e3a17ea92eb0eb
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-02-16 05:05:42 +0000

    Sync LeetCode submission Runtime - 885 ms (5.03%), Memory - 20.5 MB (100.00%)

---
+++ my-solutions/1014-k-closest-points-to-origin/solution.py
@@ -0,0 +1,27 @@
+import heapq
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        def distance(point):
+            return (point[0]**2 + point[1]**2)**0.5
+
+        distance_point_map = {}
+        minheap = []
+
+        for i in range(len(points)):
+            dist = distance(points[i])
+            if dist in distance_point_map:
+                distance_point_map[dist].append(points[i])
+            else:
+                distance_point_map[dist] = [points[i]]
+            heapq.heappush(minheap, dist)
+
+        res = []
+        iter = k
+        while iter > 0:
+            while iter > 0 and len(distance_point_map[minheap[0]]) > 0:
+                res.append(distance_point_map[minheap[0]].pop())
+                iter -= 1
+            heapq.heappop(minheap)
+        
+        return res
