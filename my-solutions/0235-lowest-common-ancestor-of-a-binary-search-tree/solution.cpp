574f4715dfb5be95292f374a18cd3e794e9bd2b0
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-03-03 01:17:32 +0000

    Sync LeetCode submission Runtime - 33 ms (7.25%), Memory - 23.4 MB (90.81%)

--- my-solutions/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.cpp
+++ my-solutions/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.cpp
@@ -11,16 +11,14 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root) {
+        if(root == nullptr)
             return nullptr;
-        } else {
-            if((root->val >= p->val && root->val <= q->val) || (root->val <= p->val && root->val >= q->val))
-                return root;
-            else if(root->val > p->val && root->val > q->val) {
-                return lowestCommonAncestor(root->left, p, q);
-            } else {
-                return lowestCommonAncestor(root->right, p, q);
-            }
-        }
+        // cout << root->val << endl;
+        if((root->val >= p->val && root->val <= q->val) || (root->val <= p->val && root->val >= q->val))
+            return root;
+        else if(root->val > p->val && root->val > q->val)
+            return lowestCommonAncestor(root->left, p, q);
+        else
+            return lowestCommonAncestor(root->right, p, q);
     }
 };
