bf384cdb450014a3c0ae6e942bd1a0c104c2c1b3
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-03-04 16:04:15 +0000

    Sync LeetCode submission Runtime - 32 ms (7.36%), Memory - 71.5 MB (99.97%)

--- my-solutions/0002-add-two-numbers/solution.cpp
+++ my-solutions/0002-add-two-numbers/solution.cpp
@@ -11,25 +11,39 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode * output = nullptr; ListNode * head = output;
-        int carry = 0;
-        int sum = 0;
-        while(l1 != nullptr || l2 != nullptr) {
-            sum = (l1 == nullptr ? 0 : l1->val) + (l2 == nullptr ? 0 : l2->val) + carry;
-            carry = sum > 9 ? 1 : 0;
-            if(output == nullptr) {
-                output = new ListNode(sum % 10); head = output;
-            }
-            else {
-               output->next = new ListNode(sum > 9 ? sum - 10 : sum); output = output->next; 
-            }
-            // cout << num1 << "&" << num2 << " ";
-            l1 != nullptr ? l1 = l1->next : nullptr;
-            l2 != nullptr ? l2 = l2->next : nullptr;
+        if(!l1 and !l2) return 0;
+        else if(!l1) return l2;
+        else if(!l2) return l1;
+
+        ListNode* l3 = new ListNode((l1->val + l2->val)%10);
+        ListNode* head = l3;
+        int carry = (l1->val + l2->val) / 10;
+        l1 = l1->next; l2 = l2->next;
+        while(l1 and l2) {
+            int sum = l1->val + l2->val + carry;
+            l3->next = new ListNode(sum % 10);
+            l3 = l3->next;
+            l1 = l1->next;
+            l2 = l2->next;
+            carry = sum / 10;
         }
-        if(carry > 0) {
-            output->next = new ListNode(carry);
-            output = output->next;
+        while(l1) {
+            int sum = l1->val + carry;
+            l3->next = new ListNode(sum % 10);
+            l3 = l3->next;
+            l1 = l1->next;
+            carry = sum / 10;
+        }
+        while(l2) {
+            int sum = l2->val + carry;
+            l3->next = new ListNode(sum % 10);
+            l3 = l3->next;
+            l2 = l2->next;
+            carry = sum / 10;
+        }
+        if(carry) {
+            l3->next = new ListNode(carry);
+            l3 = l3->next;
         }
         return head;
     }
