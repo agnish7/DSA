da0e4b68bba4dc9b198552c27ba69ad3b763ecbb
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-04-20 21:20:12 +0000

    Sync LeetCode submission Runtime - 48 ms (17.13%), Memory - 21.7 MB (99.94%)

--- my-solutions/0049-group-anagrams/solution.cpp
+++ my-solutions/0049-group-anagrams/solution.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     string getKey(string s) {
-        string key = "000000000000000000000000000";
+        string key = "00000000000000000000000000";
         for(int i = 0; i < s.length(); i++) {
-            key[int(s[i] - 'a')]++;
+            key[(int(s[i]) - int('a'))]++;
         }
         return key;
     }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
         unordered_map<string, vector<string>> string_map;
         for(int i = 0; i < size(strs); i++) {
             string key = getKey(strs[i]);
             if(string_map.find(key) == string_map.end()) {
-                vector<string> v;
-                v.push_back(strs[i]);
-                string_map[key] = v;
+                string_map[key] = vector<string> {strs[i]};
             } else {
                 string_map[key].push_back(strs[i]);
             }
         }
 
-        vector<vector<string>> res;
-        for(const auto& kv_pair: string_map) {
-            res.push_back(kv_pair.second);
+        for(const auto &i: string_map) {
+            res.push_back(i.second);
         }
+
         return res;
     }
 };
