b784b682bbab315f85a4f9b770ed78a274f774c2
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-05-23 02:12:02 +0000

    Sync LeetCode submission Runtime - 110 ms (5.14%), Memory - 21 MB (100.00%)

--- my-solutions/0347-top-k-frequent-elements/solution.py
+++ my-solutions/0347-top-k-frequent-elements/solution.py
@@ -1,31 +1,4 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # k most frequent elements
-        num_dict = {}
-        res = []
-        for i in nums:
-            if i in num_dict:
-                num_dict[i] += 1
-            else:
-                num_dict[i] = 1
-        
-        # create a reverse dict with the frequencies as the keys and the elements as the values
-        rev_dict = {}
-        for key, value in num_dict.items():
-            if value in rev_dict:
-                rev_dict[value].append(key)
-            else:
-                rev_dict[value] = [key]
-        
-        # go through the dictionary and prepare the output
-        freqs = sorted(rev_dict.keys(), reverse = True)
-
-        for freq in freqs:
-            for elem in rev_dict[freq]:
-                res.append(elem)
-                k -= 1
-                if k <= 0:
-                    return res
-
-        return res            
-
+        num_freq = Counter(nums)
+        return heapq.nlargest(k, num_freq.keys(), key=num_freq.get)
