3572bf603a4d143dd82676eb5f65b9b56bbcfb0f
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-05-31 04:38:20 +0000

    Sync LeetCode submission Runtime - 112 ms (5.14%), Memory - 21.1 MB (100.00%)

--- my-solutions/0347-top-k-frequent-elements/solution.py
+++ my-solutions/0347-top-k-frequent-elements/solution.py
@@ -1,4 +1,24 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num_freq = Counter(nums)
-        return heapq.nlargest(k, num_freq.keys(), key=num_freq.get)
+        count_nums = Counter(nums)
+        reverse_dict ={}
+        for key, value in count_nums.items():
+            if value in reverse_dict:
+                reverse_dict[value].append(key)
+            else:
+                reverse_dict[value] = [key]
+                
+        heap = list(reverse_dict.keys())
+        heap = [-i for i in heap]
+        heapq.heapify(heap)
+        result = []
+
+        while len(heap) > 0:
+            key = -heap[0]
+            heapq.heappop(heap)
+            for e in reverse_dict[key]:
+                result.append(e)
+                k -= 1
+                if k <= 0:
+                    return result
+        
