37b95c7291e7645f46198bb5c6cfe90622047f93
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-05-31 02:38:26 +0000

    Sync LeetCode submission Runtime - 55 ms (5.33%), Memory - 16.8 MB (100.00%)

--- my-solutions/0242-valid-anagram/solution.py
+++ my-solutions/0242-valid-anagram/solution.py
@@ -1,22 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dict_s = {}
-        for i in range(len(s)):
-            if s[i] in dict_s:
-                dict_s[s[i]] += 1
-            else:
-                dict_s[s[i]] = 1
-
-        for i in range(len(t)):
-            if t[i] not in dict_s:
-                return False
-            else:
-                dict_s[t[i]] -= 1
-                if dict_s[t[i]] < 0:
-                    return False
-
-        for key in dict_s:
-            if dict_s[key] != 0:
-                return False
-
-        return True
+        return Counter(s) == Counter(t)
