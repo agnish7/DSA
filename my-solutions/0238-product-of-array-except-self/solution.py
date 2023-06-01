67e09908dd316146570ed766889c055a70f6b115
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-06-01 05:08:22 +0000

    Sync LeetCode submission Runtime - 296 ms (5.05%), Memory - 24.9 MB (96.67%)

--- my-solutions/0238-product-of-array-except-self/solution.py
+++ my-solutions/0238-product-of-array-except-self/solution.py
@@ -1,16 +1,23 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        forward = copy.deepcopy(nums)
-        backward = copy.deepcopy(nums)
+        right = copy.deepcopy(nums)
         for i in range(1, len(nums)):
-            forward[i] *= forward[i-1]
-            backward[len(backward) - 1 - i] *= backward[len(backward) - i]
+            nums[i] = nums[i - 1] * nums[i]
+
+        for i in range(len(right) - 2, -1, -1):
+            right[i] = right[i] * right[i + 1]
+
+        # print(nums)
+        # print(right)
+
+        answer = [1] * len(nums)
+
+        for i in range(1, len(nums) - 1):
+            answer[i] = right[i+1] * nums[i-1]
         
-        result = copy.deepcopy(nums)
-        for i in range(1, len(result) - 1):
-            result[i] = forward[i-1] * backward[i+1]
-        
-        result[0] = backward[1]
-        result[-1] = forward[-2]
-        
-        return result
+        answer[0] = right[1]
+        answer[len(nums) - 1] = nums[len(nums) - 2]
+
+        return answer
+
+
