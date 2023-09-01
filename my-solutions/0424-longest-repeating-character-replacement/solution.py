249fb75cbc2dddf8272915265dd61165e552d772
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-09-01 06:50:03 +0000

    Sync LeetCode submission Runtime - 167 ms (16.05%), Memory - 16.4 MB (100.00%)

--- my-solutions/0424-longest-repeating-character-replacement/solution.py
+++ my-solutions/0424-longest-repeating-character-replacement/solution.py
@@ -1,24 +1,20 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        max_len, i = -1, 0
-        char_dict = {s[0]:1}
-        for j in range(1, len(s)):
-            if s[j] in char_dict.keys():
-                char_dict[s[j]] += 1
+        char_map = {}
+        l = 0
+        max_size_ss = 1
+
+        for r in range(len(s)):
+            if s[r] in char_map:
+                char_map[s[r]] += 1
             else:
-                char_dict[s[j]] = 1
-            max_freq = max(char_dict.values())
-            while j - i + 1 - max_freq > k:
-                
+                char_map[s[r]] = 1
 
-                char_dict[s[i]] -= 1
-                    
-                i+=1
-                
-                max_freq = max(char_dict.values())
-                #print(i, j, char_dict, max_freq)
+            max_char = max(list(char_map.values())) if len(list(char_map.values())) > 0 else 0
+            while(l < r and r - l + 1 - max_char > k):
+                char_map[s[l]] -= 1
+                l += 1
 
-            max_len = max(max_len, j - i + 1)
-            #print(i, j, max_len)
-        
-        return max_len
+            max_size_ss = max(max_size_ss, r - l + 1)
+
+        return max_size_ss
