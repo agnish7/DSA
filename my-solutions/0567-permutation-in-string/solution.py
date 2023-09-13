a6a381696b27809fd732ffb9599f9910c24aaf65
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-09-13 16:22:32 +0000

    Sync LeetCode submission Runtime - 69 ms (20.56%), Memory - 16.2 MB (100.00%)

--- my-solutions/0567-permutation-in-string/solution.py
+++ my-solutions/0567-permutation-in-string/solution.py
@@ -1,34 +1,38 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1_chars = [0] * 26
-        s2_chars = [0] * 26
-        l = 0
-
-        for c in s1:
-            s1_chars[ord(c) - ord('a')] += 1
-
-        def check_subset(s1_chars, s2_chars):
-            res = True
-            for i in range(len(s1_chars)):
-                res = res and (s1_chars[i] <= s2_chars[i])
-            return res
-
-        for r in range(len(s2)):
-
-            s2_chars[ord(s2[r]) - ord('a')] += 1
-            if(check_subset(s1_chars, s2_chars)):
-                # print(l, r, check_subset(s1_chars, s2_chars))
-                while(l <= r and check_subset(s1_chars, s2_chars)):
-                    s2_chars[ord(s2[l]) - ord('a')] -= 1
-                    l += 1
-                # print(l, r, s1_chars, s2_chars)
-                l -= 1
-                s2_chars[ord(s2[l]) - ord('a')] += 1
-                # print(l, r, s1_chars, s2_chars)
-            
-            if s1_chars == s2_chars:
-                return True
-        
+      if len(s1) > len(s2):
         return False
+      s1_count = [0] * 26
+      s2_count = [0] * 26
+
+      for i in range(len(s1)):
+        idx = ord(s1[i]) - ord('a')
+        s1_count[idx] += 1
+        idx = ord(s2[i]) - ord('a')
+        s2_count[idx] += 1
+      
+      matchings = sum(map(lambda i: s1_count[i] == s2_count[i], range(26)))
+
+      print(matchings, s1_count, s2_count)
+
+      for i in range(len(s1), len(s2)):
+        if matchings == 26:
+          return True
+
+        idx_l = ord(s2[i - len(s1)]) - ord('a')
+        s2_count[idx_l] -= 1
+        if s2_count[idx_l] == s1_count[idx_l]:
+          matchings += 1
+        elif s2_count[idx_l] == s1_count[idx_l] - 1: # this is so that we dont decrement matchings when there was already a mismatch and the s2_count just went down further by 1
+          matchings -= 1
+        
+        idx_r = ord(s2[i]) - ord('a')
+        s2_count[idx_r] += 1
+        if s2_count[idx_r] == s1_count[idx_r]:
+          matchings += 1
+        elif s2_count[idx_r] == s1_count[idx_r] + 1:
+          matchings -= 1
+        
+      return matchings == 26
 
 
