a48cdc08488d34d1f2f8d690dc887270e912f285
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-09-18 14:43:20 +0000

    Sync LeetCode submission Runtime - 76 ms (5.38%), Memory - 16.9 MB (100.00%)

--- my-solutions/0150-evaluate-reverse-polish-notation/solution.py
+++ my-solutions/0150-evaluate-reverse-polish-notation/solution.py
@@ -1,22 +1,27 @@
-from collections import deque
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        stack = deque()
-        for c in tokens:
-            if c in "*+/-":
-                r = int(stack.pop())
-                l = int(stack.pop())
-                res = 0
-                if c == "*":
-                    res = l * r
-                elif c == "+":
-                    res = l + r
-                elif c == "/":
-                    res = l / r
-                elif c == "-":
-                    res = l - r
-                stack.append(res)
+        evalStack = deque()
+        def performOp(op1, op2, op):
+            if op == "+":
+                return op1 + op2
+            elif op == "-":
+                return op1 - op2
+            elif op == "*":
+                return op1 * op2
             else:
-                stack.append(int(c))
-        
-        return int(stack[-1])
+                # print(op1, op2, int(op1/op2))
+                return int(op1/op2)
+        for i in range(len(tokens)):
+            if tokens[i] in ["+", "-", "*", "/"]:
+                op1 = int(evalStack[-1])
+                evalStack.pop()
+                op2 = int(evalStack[-1])
+                evalStack.pop()
+                evalStack.append(performOp(op2, op1, tokens[i]))
+            else:
+                evalStack.append(int(tokens[i]))
+            # print(evalStack)
+
+        return evalStack[-1]
+
+            
