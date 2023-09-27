083b8e37ec2a7c5effde6c00ec43bf6905aa7930
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-09-27 05:59:00 +0000

    Sync LeetCode submission Runtime - 412 ms (5.07%), Memory - 17.6 MB (100.00%)

--- my-solutions/0907-koko-eating-bananas/solution.py
+++ my-solutions/0907-koko-eating-bananas/solution.py
@@ -1,24 +1,17 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def pile_eat_time(pile_size, rate):
-            if pile_size % rate == 0:
-                return int(pile_size / rate)
-            else:
-                return pile_size // rate + 1
+        minK, maxK = 1, max(piles)
 
-        def total_eat_time(piles, k):
-            eat_times = [pile_eat_time(pile, k) for pile in piles]
-            return sum(eat_times)
+        def numHours(k):
+            return sum([int(math.ceil(x / k)) for x in piles])
 
-        l = 1
-        r = max(piles)
-        res = float('inf')
-        while(l <= r):
-            mid = (l + r) // 2
-            print(mid, total_eat_time(piles, mid))
-            if total_eat_time(piles, mid) <= h:
-                res = min(res, mid)
-                r = mid - 1
+
+        while(minK <= maxK):
+            midK = int((minK + maxK) / 2)
+            # print(minK, midK, maxK, numHours(midK))
+            if numHours(midK) > h:
+                minK = midK + 1
             else:
-                l = mid + 1        
-        return res
+                maxK = midK - 1
+
+        return minK
