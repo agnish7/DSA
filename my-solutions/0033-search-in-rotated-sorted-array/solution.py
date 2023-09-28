4873112f3528eca1e38c4911a2aadbac8d5e75eb
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-09-28 08:01:38 +0000

    Sync LeetCode submission Runtime - 44 ms (0.12%), Memory - 16.9 MB (100.00%)

--- my-solutions/0033-search-in-rotated-sorted-array/solution.py
+++ my-solutions/0033-search-in-rotated-sorted-array/solution.py
@@ -1,22 +1,35 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
-
-        while l <= r:
-            mid = (l + r) // 2
-            if target == nums[mid]:
-                return mid
-
-            # left sorted portion
-            if nums[l] <= nums[mid]:
-                if target > nums[mid] or target < nums[l]:
-                    l = mid + 1
-                else:
-                    r = mid - 1
-            # right sorted portion
-            else:
-                if target < nums[mid] or target > nums[r]:
-                    r = mid - 1
-                else:
-                    l = mid + 1
+      def binSearch(l, r, target):
+        while(l <= r):
+          mid = int((l + r) / 2)
+          if nums[mid] == target:
+            return mid
+          elif nums[mid] > target:
+            r = mid - 1
+          else:
+            l = mid + 1
         return -1
+      
+      if nums[len(nums) - 1] > nums[0]:
+        return binSearch(0, len(nums) - 1, target)
+
+      def searchTarget(l, r, target):
+        if r - l <= 2:
+          return nums.index(target) if target in nums else -1
+
+        mid = int((l + r)/2)
+
+        print(l, mid, r)
+
+        if nums[mid] == target:
+          return mid
+        if nums[mid] < nums[l]:
+          postMid = binSearch(mid + 1, r, target)
+          return postMid if postMid != -1 else searchTarget(l, mid - 1, target)
+        elif nums[mid] > nums[l]:
+          preMid = binSearch(l, mid - 1, target)
+          return preMid if preMid != -1 else searchTarget(mid + 1, r, target)
+
+      return searchTarget(0, len(nums) - 1, target)
+
