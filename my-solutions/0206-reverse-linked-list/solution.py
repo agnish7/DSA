4039d6bf2ad101290eb9be9a59eb807c2bf4e188
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-10-18 15:05:24 +0000

    Sync LeetCode submission Runtime - 39 ms (0.57%), Memory - 22.8 MB (7.69%)

--- my-solutions/0206-reverse-linked-list/solution.py
+++ my-solutions/0206-reverse-linked-list/solution.py
@@ -5,12 +5,11 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head == None:
-            return None
-        elif head.next == None:
+        if not head or not head.next:
             return head
         else:
-            temp = self.reverseList(head.next)
+            newHead = self.reverseList(head.next)
             head.next.next = head
             head.next = None
-            return temp
+            return newHead
+
