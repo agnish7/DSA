84b5cf46c6904a97b5f2d6ea2c233b53af55229d
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-10-19 15:10:55 +0000

    Sync LeetCode submission Runtime - 91 ms (5.04%), Memory - 36.6 MB (7.13%)

--- my-solutions/0143-reorder-list/solution.py
+++ my-solutions/0143-reorder-list/solution.py
@@ -8,40 +8,38 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        
-        if(head == None or head.next == None):
-            return head
-        
-        # find the middle of the list
-        fast = head
-        slow = head
-        while(fast.next != None and fast.next.next != None):
+        fast, slow = head, head
+        while(fast and fast.next):
             fast = fast.next.next
-            slow = slow.next    
-        middle = slow
-        
-        # split the list into 2
+            slow = slow.next
+
         list1 = head
-        list2 = middle.next
-        middle.next = None
+        list2 = slow.next
+        slow.next = None
+
+        # function to reverse a list
+        def reverseList(head):
+            if not head or not head.next:
+                return head
+            else:
+                newHead = reverseList(head.next)
+                head.next.next = head
+                head.next = None
+                return newHead
         
-        # reverse the second list
-        prev = None
-        curr = list2
-        while(curr.next != None):
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
-        curr.next = prev
-        list2 = curr
-        
-        # combine the 2 lists into 1
-        while(list1 != None and list2 != None):
-            t1 = list1.next
-            list1.next = list2
-            list1 = t1
-            
-            t2 = list2.next
-            list2.next = list1
-            list2 = t2
+        list2 = reverseList(list2)
+
+        # merge the 2 lists
+        prev1, prev2 = None, None
+        while(list1 and list2):
+            prev1 = list1
+            list1 = list1.next
+
+            prev1.next = list2
+
+            prev2 = list2
+            list2 = list2.next
+
+            prev2.next = list1
+
+
