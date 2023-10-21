2e9c9a4cb3fb6add3180690c8c644b685a640300
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-10-21 15:05:35 +0000

    Sync LeetCode submission Runtime - 67 ms (7.00%), Memory - 16.5 MB (100.00%)

--- my-solutions/0002-add-two-numbers/solution.py
+++ my-solutions/0002-add-two-numbers/solution.py
@@ -5,49 +5,41 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        if not l1 and not l2:
-            return ListNode(0)
-        if not l1:
-            return l2
-        if not l2:
-            return l1
-        
-        s, c = (l1.val + l2.val) % 10, (l1.val + l2.val) // 10
-        res = ListNode(s)
-        res.next = None
-        head = res
-        l1, l2 = l1.next, l2.next
+        summa, carry = 0, 0
+        res, head = None, None
 
         while(l1 and l2):
-            
-            s = (l1.val + l2.val + c)
-            d = s % 10
-            c = s // 10
-            # print(l1.val, l2.val, s, c)
-            res.next = ListNode(d)
-            res = res.next
+            summa = (l1.val + l2.val + carry) % 10
+            newNode = ListNode(summa)
+            if res:
+                res.next = newNode
+                res = res.next
+            else:
+                res = newNode
+                head = res
+            carry = (l1.val + l2.val + carry) // 10
             l1, l2 = l1.next, l2.next
-        
+
         while(l1):
-            s = (l1.val + c)
-            d = s % 10
-            c = s // 10
-            res.next = ListNode(d)
+            summa = (l1.val + carry) % 10
+            newNode = ListNode(summa)
+            res.next = newNode
             res = res.next
+            carry = (l1.val + carry) // 10
             l1 = l1.next
         
         while(l2):
-            s = (l2.val + c)
-            d = s % 10
-            c = s // 10
-            res.next = ListNode(d)
+            summa = (l2.val + carry) % 10
+            newNode = ListNode(summa)
+            res.next = newNode
             res = res.next
+            carry = (l2.val + carry) // 10
             l2 = l2.next
+
+        if carry:
+            res.next = ListNode(carry)
         
-        if c:
-            res.next = ListNode(c)
-            res = res.next
-        
-        res.next = None
+        res = None
 
         return head
+        
