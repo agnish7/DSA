44cd73451f911b1ea8cbc23c9462e7106b9c2d16
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-11-24 16:52:59 +0000

    Sync LeetCode submission Runtime - 104 ms (9.95%), Memory - 20.9 MB (89.80%)

--- my-solutions/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
+++ my-solutions/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
@@ -1,34 +1,25 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        pre = 0
+        def build(ino_l, ino_r):
+            nonlocal pre
+            if ino_l > ino_r:
+                return None
 
-    def buildTree(self, preorder, inorder):
-        """
-        :type preorder: List[int]
-        :type inorder: List[int]
-        :rtype: TreeNode
-        """
+            root = TreeNode(preorder[pre])
+            idx = inorder.index(preorder[pre])
+            pre += 1
+            left = build(ino_l, idx - 1)
+            right = build(idx + 1, ino_r)
+            root.left = left
+            root.right = right
+            return root
 
-        if len(preorder) == 0:
-            return None
-        if len(preorder) == 1:
-            return TreeNode(preorder[0])
-        
-        if preorder[0] not in inorder:
-            return None
-        root_idx = inorder.index(preorder[0])
-
-        left_tree = self.buildTree(preorder[1:root_idx+1], inorder[:root_idx])
-        right_tree = self.buildTree(preorder[root_idx+1:], inorder[root_idx+1:])
-
-        root = TreeNode(preorder[0])
-
-        root.left = left_tree
-        root.right = right_tree
-
-        return root
+        return build(0, len(inorder) - 1)
 
