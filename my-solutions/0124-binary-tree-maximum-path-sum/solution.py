4dbf139ebaa7a5120fedd694d38f81b7e88e21f6
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2023-11-27 07:40:25 +0000

    Sync LeetCode submission Runtime - 85 ms (6.02%), Memory - 23.9 MB (31.16%)

--- my-solutions/0124-binary-tree-maximum-path-sum/solution.py
+++ my-solutions/0124-binary-tree-maximum-path-sum/solution.py
@@ -5,22 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def maxPath(self, root):
-        if not root:
-            return[float("-inf"), float("-inf")]
-        
-        if not root.left and not root.right:
-            return [root.val, root.val]
-
-        left = self.maxPath(root.left)
-        right = self.maxPath(root.right)
-
-        # include parent max(left include, right include) + root.val
-        res = [max(max(left[0], right[0]) + root.val, root.val)]
-        # not include parent max(left not include, right not include, root val, root val + left include + right include)
-        res.append(max(left[1], right[1], root.val, root.val + left[0] + right[0], root.val + left[0], root.val + right[0]))
-
-        return res
-
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        return max(self.maxPath(root))
+
+        maxPath = float('-inf')
+        
+        def dfs(node):
+            nonlocal maxPath
+            if not node:
+                return float('-inf')
+
+            l = dfs(node.left)
+            r = dfs(node.right)
+
+            maxPath = max(maxPath, l, r, node.val + max(l, 0) + max(r, 0))
+            return node.val + max(l, r, 0)
+
+        dfs(root)
+
+        return maxPath
+            
