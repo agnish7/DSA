240e3e74e061318d9eae5b3a00012de4386328b8
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-02-22 11:50:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 16.7 MB (11.91%)

--- my-solutions/1029-vertical-order-traversal-of-a-binary-tree/solution.cpp
+++ my-solutions/1029-vertical-order-traversal-of-a-binary-tree/solution.cpp
@@ -12,32 +12,46 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, vector<int>>> res;
-        dfs(res, root, 0, 0);
-        vector<vector<int>> output;
-        for(auto& p1: res) {
-            vector<int> temp;
-            for(auto& p2: p1.second) {
-                // cout << p1.first << " " << p2.first << " ";
-                // for(int i = 0; i < p2.second.size(); ++i) {
-                //     cout << p2.second[i] << " ";
-                // }
-                // cout << endl;
-                sort(p2.second.begin(), p2.second.end());
-                for(auto& e: p2.second) {
-                    temp.push_back(e);
+        unordered_map<int, unordered_map<int, vector<int>>> nodeMap;
+        int minCol = 0, maxCol = 0, minRow = 0, maxRow = 0;
+        vector<vector<int>> res;
+
+        dfs(nodeMap, minCol, maxCol, minRow, maxRow, 0, 0, root);
+
+        for(int i = minCol; i <= maxCol; ++i) {
+            vector<int> rowElements;
+            for(int j = minRow; j <= maxRow; ++j) {
+                sort(nodeMap[i][j].begin(), nodeMap[i][j].end());
+                // cout << i << " " << j << ": ";
+                for(auto& e : nodeMap[i][j]) {
+                    rowElements.push_back(e);
+                    // cout << e << " ";
                 }
+                // cout << endl;
             }
-            output.push_back(temp);
+            res.push_back(rowElements);
         }
-        return output;
+        return res;
     }
-    void dfs(map<int, map<int, vector<int>>>& res, TreeNode* root, int r, int c) {
-        if(!root) {
+    void dfs(
+        unordered_map<int, unordered_map<int, vector<int>>>& nodeMap,
+        int& minCol,
+        int& maxCol,
+        int& minRow,
+        int& maxRow,
+        int c,
+        int r,
+        TreeNode* currNode
+    ) {
+        if(!currNode) {
             return;
         }
-        res[c][r].push_back(root->val);
-        dfs(res, root->left, r + 1, c - 1);
-        dfs(res, root->right, r + 1, c + 1);
+        maxCol = max(c, maxCol);
+        minCol = min(c, minCol);
+        maxRow = max(r, maxRow);
+        minRow = min(r, minRow);
+        nodeMap[c][r].push_back(currNode->val);
+        dfs(nodeMap, minCol, maxCol, minRow, maxRow, c - 1, r + 1, currNode->left);
+        dfs(nodeMap, minCol, maxCol, minRow, maxRow, c + 1, r + 1, currNode->right);
     }
 };
