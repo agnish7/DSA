dce1abecd5a0faa189898700aed33d0e89ff586d
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-02-26 13:09:04 +0000

    Sync LeetCode submission Runtime - 26 ms (70.86%), Memory - 59.3 MB (77.93%)

--- my-solutions/0215-kth-largest-element-in-an-array/solution.cpp
+++ my-solutions/0215-kth-largest-element-in-an-array/solution.cpp
@@ -1,34 +1,7 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int l = 0, r = nums.size() - 1;
-
-        while(l <= r) {
-            int i = l, m = l, j = r;
-            int p = l + rand() % (r - l + 1);
-            int pivot = nums[p];
-            while(m <= j) {
-                if(nums[m] > pivot) {
-                    swap(nums[m], nums[i]);
-                    ++m;
-                    ++i;
-                } else if(nums[m] < pivot) {
-                    swap(nums[m], nums[j]);
-                    --j;
-                } else {
-                    ++m;
-                }
-            }
-
-            if(k >= i + 1 && k <= j + 1) {
-                return nums[i];
-            } else if (k < i) {
-                r = i - 1;
-            } else if (k > j) {
-                l = j + 1;
-            }
-        }
-
-        return INT_MIN;
+        sort(nums.begin(), nums.end(), greater<int>());
+        return nums[k - 1];
     }
 };
