4fc0c8ff9c8be69bcaaf6c486fba23e63f3243ea
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-03-15 12:32:58 +0000

    Sync LeetCode submission Runtime - 1317 ms (5.00%), Memory - 350.7 MB (5.00%)

--- my-solutions/1014-k-closest-points-to-origin/solution.cpp
+++ my-solutions/1014-k-closest-points-to-origin/solution.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        unordered_map<int, vector<vector<int>>> dist_map;
-        priority_queue<int> pq;
-        vector<vector<int>> result;
-        for(int i = 0; i < size(points); i++) {
-            int distance = pow(points[i][0], 2) + pow(points[i][1], 2);
-            pq.push(-distance);
-            dist_map[-distance].push_back(points[i]);
+        auto dist = [](vector<int> a) {
+            return a[0] * a[0]  + a[1] * a[1];
+        };
+        auto cmp = [dist](vector<int> a, vector<int> b){
+            return dist(a) < dist(b);
+        };
+        priority_queue<vector<int>, vector<vector<int>>, decltype(cmp)> kClosest(cmp);
+        vector<vector<int>> res;
+        kClosest.push(points[0]);
+        for(int i = 1; i < points.size(); ++i) {
+            if(dist(points[i]) < dist(kClosest.top()) || kClosest.size() < k) kClosest.push(points[i]);
+            if(kClosest.size() > k) kClosest.pop();
         }
-
-        while(!pq.empty()) {
-            // cout << pq.top() << endl;
-            vector<vector<int>> curr_points = dist_map[pq.top()];
-            for(int i = 0; i < size(curr_points); i++) {
-                result.push_back(curr_points[i]);
-                k--;
-                if(k == 0) return result;
-            }
-            pq.pop();
+        while(!kClosest.empty()) {
+            res.push_back(kClosest.top());
+            kClosest.pop();
         }
-
-        return vector<vector<int>>({{}});
+        return res;
     }
 };
