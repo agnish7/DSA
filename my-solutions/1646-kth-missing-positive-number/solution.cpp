38090505a4071baa4a53f947a282abf7f2876040
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-03-17 05:24:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.3 MB (27.44%)

---
+++ my-solutions/1646-kth-missing-positive-number/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int l = 0, r = arr.size() - 1;
+        while(l <= r) {
+            int mid = (l + r) / 2;
+            if(arr[mid] - mid - 1 < k) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return k + r + 1;
+    }
+};
