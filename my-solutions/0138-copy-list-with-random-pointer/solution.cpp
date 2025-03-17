416484bdbfb7e656321226bef953292a3e7b4b96
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-03-17 12:13:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.3 MB (26.98%)

--- my-solutions/0138-copy-list-with-random-pointer/solution.cpp
+++ my-solutions/0138-copy-list-with-random-pointer/solution.cpp
@@ -17,27 +17,21 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*, Node*> nodeMap;
-
-        Node* curr = head;
-        while(curr) {
-            if(nodeMap.find(curr) == nodeMap.end()) {
-                nodeMap[curr] = new Node(curr->val);
+        unordered_map<Node*, Node*> nodeMap{};
+        Node* prev = nullptr;
+        Node* res = head;
+        while(head) {
+            if(!nodeMap.count(head)) {
+                nodeMap[head] = new Node(head->val);
             }
-            if(nodeMap.find(curr->next) == nodeMap.end()) {
-                if(curr->next) nodeMap[curr->next] = new Node(curr->next->val); else nodeMap[curr->next] = nullptr;
-                nodeMap[curr]->next = nodeMap[curr->next];
-            } else {
-                nodeMap[curr]->next = nodeMap[curr->next];
+            if(head->random && !nodeMap.count(head->random)) {
+                nodeMap[head->random] = new Node(head->random->val);
             }
-            if(nodeMap.find(curr->random) == nodeMap.end()) {
-                if(curr->random) nodeMap[curr->random] = new Node(curr->random->val); else nodeMap[curr->random] = nullptr;
-                nodeMap[curr]->random = nodeMap[curr->random];
-            } else {
-                nodeMap[curr]->random = nodeMap[curr->random];
-            }
-            curr = curr->next;
+            nodeMap[head]->random = nodeMap[head->random];
+            if(prev) nodeMap[prev]->next = nodeMap[head];
+            prev = head;
+            head = head->next;
         }
-        return nodeMap[head];
+        return nodeMap[res];
     }
 };
