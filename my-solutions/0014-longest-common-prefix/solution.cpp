c8871a545b4c7b9fcecea720c8ddcaaa6c80c744
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-03-18 05:33:13 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.5 MB (5.43%)

--- my-solutions/0014-longest-common-prefix/solution.cpp
+++ my-solutions/0014-longest-common-prefix/solution.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string start = strs[0];
-
-        for(int i = 1; i < strs.size(); ++i) {
-            int j = 0, n = strs[i].size();
-            while(j < n && strs[i][j] == start[j]) {
-                ++j;
+        return helper(strs, 0, strs.size() - 1);
+    }
+    string helper(vector<string>& strs, int l, int r) {
+        if(l == r) return strs[l];
+        if(l + 1 == r) {
+            string res = "";
+            int i = 0;
+            while(i < strs[l].size() && i < strs[r].size()) {
+                if(strs[l][i] == strs[r][i]) res += strs[l][i++];
+                else break;
             }
-            start = start.substr(0, j);
+            // cout << l << " " << r << " " << res << endl;
+            return res;
         }
-        return start;
+        int m = (l + r) / 2;
+        string left = helper(strs, l, m);
+        string right = helper(strs, m + 1, r);
+        // cout << l << " " << r << " " << left << " " << right << endl;
+        string res = "";
+        int i = 0;
+        while(i < left.size() && i < right.size()) {
+            if(left[i] == right[i]) res += left[i++];
+            else break;
+        }
+        return res;
     }
 };
