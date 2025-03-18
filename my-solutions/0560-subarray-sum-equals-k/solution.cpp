65632ac4a319b6a611c96d17eae5190402a3c3b0
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-03-18 04:19:37 +0000

    Sync LeetCode submission Runtime - 59 ms (33.96%), Memory - 51 MB (12.57%)

--- my-solutions/0560-subarray-sum-equals-k/solution.cpp
+++ my-solutions/0560-subarray-sum-equals-k/solution.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> sumMap{{0,1}};
         int sum = 0;
-        unordered_multiset<int> prefix;
-        prefix.insert(0);
         int res = 0;
-        for(int i = 0; i < nums.size(); ++i) {
-            sum += nums[i];
-            if(prefix.count(sum - k)) {
-                res += prefix.count(sum - k);
-            }
-            prefix.insert(sum);
+        for(auto& num: nums) {
+            sum += num;
+            res += sumMap[sum-k];
+            ++sumMap[sum];
         }
         return res;
     }
