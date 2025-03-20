09490c13a725653058ce726c45454d6a9ecacb83
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-03-20 07:46:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.7 MB (69.97%)

---
+++ my-solutions/1128-remove-all-adjacent-duplicates-in-string/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res = "";
+        for(int i = 0; i < s.size(); ++i) {
+            if(!res.empty() && s[i] == res.back()) {
+                res.pop_back();
+            } else {
+                res.push_back(s[i]);
+            }
+        }
+        return res;
+    }
+};
