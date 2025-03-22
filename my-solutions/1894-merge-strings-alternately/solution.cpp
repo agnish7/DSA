bde134166614559cfb587d27798c2d32b1842562
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-03-22 05:13:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.5 MB (63.77%)

---
+++ my-solutions/1894-merge-strings-alternately/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res{};
+        int i = 0, j = 0;
+        while(i < word1.size() && j < word2.size()) {
+            if((i+j)%2 == 0) res+=word1[i++];
+            else res+=word2[j++];
+        }
+        if(i<word1.size())res+=word1.substr(i, word1.size() - i);
+        if(j<word2.size())res+=word2.substr(j, word2.size() - j);
+        return res;
+    }
+};
