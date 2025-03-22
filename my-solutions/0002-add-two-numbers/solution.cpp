a813d6dd12ab0b6a924ef8d9aefb3e99eb82318e
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-03-22 05:23:15 +0000

    Sync LeetCode submission Runtime - 2 ms (39.04%), Memory - 77.3 MB (11.65%)

--- my-solutions/0002-add-two-numbers/solution.cpp
+++ my-solutions/0002-add-two-numbers/solution.cpp
@@ -11,40 +11,20 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if(!l1 and !l2) return 0;
-        else if(!l1) return l2;
-        else if(!l2) return l1;
+        int sum = 0, carry = 0;
+        ListNode* i = l1, *j = l2;
+        ListNode head = ListNode(0);
+        ListNode* ptr = &head;
+        while(i || j || carry) {
+            sum = (i?i->val:0) + (j?j->val:0) + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            ptr->next = new ListNode(sum);
+            ptr = ptr->next;
+            if(i) i = i->next;
+            if(j) j = j->next;
+        }
 
-        ListNode* l3 = new ListNode((l1->val + l2->val)%10);
-        ListNode* head = l3;
-        int carry = (l1->val + l2->val) / 10;
-        l1 = l1->next; l2 = l2->next;
-        while(l1 and l2) {
-            int sum = l1->val + l2->val + carry;
-            l3->next = new ListNode(sum % 10);
-            l3 = l3->next;
-            l1 = l1->next;
-            l2 = l2->next;
-            carry = sum / 10;
-        }
-        while(l1) {
-            int sum = l1->val + carry;
-            l3->next = new ListNode(sum % 10);
-            l3 = l3->next;
-            l1 = l1->next;
-            carry = sum / 10;
-        }
-        while(l2) {
-            int sum = l2->val + carry;
-            l3->next = new ListNode(sum % 10);
-            l3 = l3->next;
-            l2 = l2->next;
-            carry = sum / 10;
-        }
-        if(carry) {
-            l3->next = new ListNode(carry);
-            l3 = l3->next;
-        }
-        return head;
+        return head.next;
     }
 };
