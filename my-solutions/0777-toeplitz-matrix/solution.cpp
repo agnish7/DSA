551bafbb52ee1657b4a209a445889dda66d956c1
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-04-04 11:57:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.2 MB (27.39%)

--- my-solutions/0777-toeplitz-matrix/solution.cpp
+++ my-solutions/0777-toeplitz-matrix/solution.cpp
@@ -1,29 +1,11 @@
 class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-        for(int c = 0; c < n; ++c) {
-            int r = 0, c_bar = c;
-            int prev = matrix[r][c];
-            while(c_bar < n && r < m) {
-                if(prev != matrix[r][c_bar]) return false;
-                prev = matrix[r][c_bar];
-                ++c_bar;
-                ++r;
+        for(int i = 0; i < matrix.size() - 1; ++i) {
+            for(int j = 0; j < matrix[0].size() - 1; ++j) {
+                if(matrix[i][j] != matrix[i + 1][j + 1]) return false;
             }
         }
-
-        for(int r = 0; r < m; ++r) {
-            int c = 0, r_bar = r;
-            int prev = matrix[r_bar][c];
-            while(r_bar < m & c < n) {
-                if(prev != matrix[r_bar][c]) return false;
-                prev = matrix[r_bar][c];
-                ++r_bar;
-                ++c;
-            }
-        }
-
         return true;
     }
 };
