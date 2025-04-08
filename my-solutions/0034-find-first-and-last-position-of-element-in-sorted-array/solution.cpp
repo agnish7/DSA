5858ddd8602bf4be13c30b54142bf80c0342fb9d
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-04-08 07:45:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.5 MB (88.10%)

--- my-solutions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ my-solutions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -1,29 +1,8 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1;
-        vector<int> res;
-        while(l <= r) {
-            int mid = (l + r) / 2;
-            if(nums[mid] >= target) {
-                r = mid - 1; 
-            } else {
-                l = mid + 1;
-            }
-        }
-        int start = l;
-
-        l = 0, r = nums.size() - 1;
-        while(l <= r) {
-            int mid = (l + r) / 2;
-            if(nums[mid] <= target) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        }
-        int end = l;
-
-        return start == end ? vector<int>{-1, -1} : vector<int>{start, end - 1};
+        auto [start, end] = equal_range(nums.begin(), nums.end(), target);
+        if(start == end) return {-1, -1};
+        return {static_cast<int>(distance(nums.begin(), start)), static_cast<int>(distance(nums.begin(), end) - 1)};
     }
 };
