89c65710921cd8be606339cf260c7017bd96cfba
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-04-14 15:55:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.2 MB (10.86%)

--- my-solutions/0670-maximum-swap/solution.cpp
+++ my-solutions/0670-maximum-swap/solution.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     int maximumSwap(int num) {
-        int res = 0;
-        vector<int> numArray;
-        while(num) {
-            numArray.push_back(num%10);
-            num/=10;
-        }
-
-        int max_i = 0;
-        int swap_i = -1, swap_j = -1;
-
-        // for(auto& i: numArray) cout << i << " ";
-        // cout << endl;
-
-        for(int i = 1; i < numArray.size(); ++i) {
-            if(numArray[i] > numArray[max_i]) max_i = i;
-            if(numArray[i] < numArray[max_i]) {
-                swap_i = max_i;
-                swap_j = i;
+        string numAsString = to_string(num);
+        vector<int> maxRight(numAsString.size(), 0);
+        
+        int maxR = INT_MIN;
+        int maxRIdx = -1;
+        for(int i = numAsString.size() - 1; i > -1; --i) {
+            maxRight[i] = maxRIdx;
+            if(numAsString[i] - '0' > maxR) {
+                maxR = numAsString[i] - '0';
+                maxRIdx = i;
             }
         }
 
-        //cout << swap_i << " " << swap_j << endl;
+        for(int i = 0; i < numAsString.size() - 1; ++i) {
+            int idx = maxRight[i];
+            if(numAsString[idx] - '0' > numAsString[i] - '0') {
+                swap(numAsString[idx], numAsString[i]);
+                break;
+            }
+        }
 
-        if(swap_i != -1 && swap_j != -1) swap(numArray[swap_i], numArray[swap_j]);
-        for(int i = numArray.size()-1; i > -1; --i) res = res * 10 + numArray[i];
-        return res;
+        return stoi(numAsString);
     }
 };
