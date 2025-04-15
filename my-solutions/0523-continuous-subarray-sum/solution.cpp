65af593d2fe78302862c350fdc83c1570b34afb4
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-04-15 16:38:28 +0000

    Sync LeetCode submission Runtime - 79 ms (50.32%), Memory - 146.3 MB (81.00%)

--- my-solutions/0523-continuous-subarray-sum/solution.cpp
+++ my-solutions/0523-continuous-subarray-sum/solution.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> cache({{0,-1}});
+        unordered_map<int, int> remainder_to_index;
         int sum = 0;
         for(int i = 0; i < nums.size(); ++i) {
             sum += nums[i];
-            if(k != 0) sum %= k;
-            if(cache.count(sum) && i - cache[sum] >= 2) return true;
-            if(!cache.count(sum)) cache[sum] = i;
+            int remainder = sum % k;
+
+            if(remainder == 0 && i >= 1) return true;
+            if(remainder_to_index.count(remainder) && remainder_to_index[remainder] < i - 1) return true;
+            if(!remainder_to_index.count(remainder)) remainder_to_index[remainder] = i;
         }
+
         return false;
     }
 };
