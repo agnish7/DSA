eea9c2b70e568654fcb876103d3f71f5a0cbd0d4
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-04-16 05:40:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.6 MB (84.04%)

--- my-solutions/1029-vertical-order-traversal-of-a-binary-tree/solution.cpp
+++ my-solutions/1029-vertical-order-traversal-of-a-binary-tree/solution.cpp
@@ -6,52 +6,49 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        unordered_map<int, unordered_map<int, vector<int>>> nodeMap;
-        int minCol = 0, maxCol = 0, minRow = 0, maxRow = 0;
-        vector<vector<int>> res;
+        unordered_map < int, vector<pair<int, int>>> col_to_data;
 
-        dfs(nodeMap, minCol, maxCol, minRow, maxRow, 0, 0, root);
+        int minCol = 0, maxCol = 0;
 
-        for(int i = minCol; i <= maxCol; ++i) {
-            vector<int> rowElements;
-            for(int j = minRow; j <= maxRow; ++j) {
-                sort(nodeMap[i][j].begin(), nodeMap[i][j].end());
-                // cout << i << " " << j << ": ";
-                for(auto& e : nodeMap[i][j]) {
-                    rowElements.push_back(e);
-                    // cout << e << " ";
-                }
-                // cout << endl;
+        queue<tuple<TreeNode*, int, int>> q;
+        q.push(tuple(root, 0, 0));
+
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; ++i) {
+                auto [node, r, c] = q.front();
+                q.pop();
+
+                minCol = min(minCol, c);
+                maxCol = max(maxCol, c);
+
+                col_to_data[c].emplace_back(r, node->val);
+                if (node->left)
+                    q.push(tuple(node->left, r + 1, c - 1));
+                if (node->right)
+                    q.push(tuple(node->right, r + 1, c + 1));
             }
-            res.push_back(rowElements);
         }
+
+        vector<vector<int>> res;
+        for (int c = minCol; c <= maxCol; ++c) {
+            // Sort by row, then value
+            sort(col_to_data[c].begin(), col_to_data[c].end());
+            vector<int> vertical;
+            vertical.reserve(col_to_data[c].size());
+            for (auto& [row, val] : col_to_data[c]) {
+                vertical.push_back(val);
+            }
+            res.push_back(vertical);
+        }
+
         return res;
-    }
-    void dfs(
-        unordered_map<int, unordered_map<int, vector<int>>>& nodeMap,
-        int& minCol,
-        int& maxCol,
-        int& minRow,
-        int& maxRow,
-        int c,
-        int r,
-        TreeNode* currNode
-    ) {
-        if(!currNode) {
-            return;
-        }
-        maxCol = max(c, maxCol);
-        minCol = min(c, minCol);
-        maxRow = max(r, maxRow);
-        minRow = min(r, minRow);
-        nodeMap[c][r].push_back(currNode->val);
-        dfs(nodeMap, minCol, maxCol, minRow, maxRow, c - 1, r + 1, currNode->left);
-        dfs(nodeMap, minCol, maxCol, minRow, maxRow, c + 1, r + 1, currNode->right);
     }
 };
