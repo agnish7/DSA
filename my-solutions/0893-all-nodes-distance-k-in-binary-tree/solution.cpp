e11078249c046c384649868329fcccfa9b2adb60
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-04-16 16:45:15 +0000

    Sync LeetCode submission Runtime - 8 ms (23.85%), Memory - 15.2 MB (83.88%)

--- my-solutions/0893-all-nodes-distance-k-in-binary-tree/solution.cpp
+++ my-solutions/0893-all-nodes-distance-k-in-binary-tree/solution.cpp
@@ -8,32 +8,49 @@
  * };
  */
 class Solution {
+    unordered_map<TreeNode*, TreeNode*> parent;
+
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<TreeNode*, TreeNode*> parent;
-        dfs(root, nullptr, parent);
-        TreeNode* curr = target;
-        unordered_set<TreeNode*> visited;
-        vector<int> res;
-        while(curr) {
-            findNodes(curr, k--, visited, res);
-            curr = parent[curr];
+        queue<TreeNode*> q;
+        q.push(target);
+
+        parent[root] = nullptr;
+
+        dfs(root);
+
+        int distance = 0;
+
+        while(!q.empty()) {
+
+            if(distance == k) break;
+            int size = q.size();
+            for(int i = 0; i < size; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+                node->val = -1;
+
+                if(node->left && node->left->val != -1) q.push(node->left);
+                if(node->right && node->right->val != -1) q.push(node->right);
+                if(parent[node] && parent[node]->val != -1) q.push(parent[node]);
+            }
+            ++distance;
         }
+
+        vector<int> res;
+        while(!q.empty()) {res.push_back(q.front()->val); q.pop();}
         return res;
     }
-
-    void dfs(TreeNode* root, TreeNode* prev, unordered_map<TreeNode*, TreeNode*>& parent) {
+    void dfs(TreeNode* root) {
         if(!root) return;
-        parent[root] = prev;
-        dfs(root->left, root, parent);
-        dfs(root->right, root, parent);
+        if(root->left) {
+            parent[root->left] = root;
+            dfs(root->left);
+        }
+        if(root->right) {
+            parent[root->right] = root;
+            dfs(root->right);
+        }
     }
 
-    void findNodes(TreeNode* node, int k, unordered_set<TreeNode*>& visited, vector<int>& res) {
-        if(!node || k < 0 || visited.count(node)) return;
-        visited.insert(node);
-        if(k == 0) res.push_back(node->val);
-        findNodes(node->left, k - 1, visited, res);
-        findNodes(node->right, k - 1, visited, res);
-    }
 };
