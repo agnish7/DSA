1f20147833d9bbbd3547ba19a1f53863db16c5c3
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-04-16 14:21:37 +0000

    Sync LeetCode submission Runtime - 6 ms (36.54%), Memory - 22 MB (92.58%)

--- my-solutions/0680-valid-palindrome-ii/solution.cpp
+++ my-solutions/0680-valid-palindrome-ii/solution.cpp
@@ -2,40 +2,21 @@ class Solution {
 public:
     bool validPalindrome(string s) {
         int l = 0, r = s.size() - 1;
+        bool flag = false;
         while(l < r) {
             if(s[l] == s[r]) {
                 ++l;
                 --r;
-            } else {
-                int l1 = l + 1, r1 = r;
-                int l2 = l, r2 = r - 1;
-                bool res1 = true, res2 = true;
-                while(l1 < r1) {
-                    // cout << l1 << " " << r1 << endl;
-                    if(s[l1] == s[r1]) {
-                        ++l1;
-                        --r1;
-                    } else {
-                        res1 = false;
-                        break;
-                    }
-                }
-                while(l2 < r2) {
-                    // cout << l2 << " " << r2 << endl;
-                    if(s[l2] == s[r2]) {
-                        ++l2;
-                        --r2;
-                    } else {
-                        res2 = false;
-                        break;
-                    }
-                }
-                if(!res1 && !res2) {
-                    return false;
-                } else {
-                    return true;
-                }
-            }
+            } else break;
+        }
+        return isPalindrome(s, l + 1, r) || isPalindrome(s, l, r - 1);
+    }
+
+    bool isPalindrome(string& s, int l, int r) {
+        while(l < r) {
+            if(s[l] != s[r]) return false;
+            ++l;
+            --r;
         }
         return true;
     }
