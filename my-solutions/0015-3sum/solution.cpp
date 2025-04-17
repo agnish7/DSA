ff39cf3514555dbd497d7a76aef8c0e5ccb696d4
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-04-17 06:04:22 +0000

    Sync LeetCode submission Runtime - 55 ms (27.99%), Memory - 29.1 MB (44.47%)

--- my-solutions/0015-3sum/solution.cpp
+++ my-solutions/0015-3sum/solution.cpp
@@ -3,20 +3,25 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<vector<int>> res;
-        for(int i = 0; i < nums.size() - 2; ++i) {
-            if(i > 0 && nums[i] == nums[i-1]) continue;
+        for(int i = 0; i < nums.size(); ++i) {
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
             int l = i + 1, r = nums.size() - 1;
             while(l < r) {
-                int sum = nums[i] + nums[l] + nums[r]; // Store sum for clarity and efficiency
+                if(l > i + 1 && nums[l] == nums[l - 1]) {
+                    ++l;
+                    continue;
+                }
+                int sum = nums[i] + nums[l] + nums[r];
                 if(sum == 0) {
                     res.push_back({nums[i], nums[l], nums[r]});
                     ++l;
-                    while(l < r && nums[l] == nums[l - 1]) ++l;
                     --r;
-                    while(l < r && nums[r] == nums[r + 1]) --r;
                 }
-                else if(sum > 0) --r;
-                else ++l;
+                else if(sum < 0) {
+                    ++l;
+                } else {
+                    --r;
+                }
             }
         }
         return res;
