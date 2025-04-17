d557e36aa521939ff81bf674fca0bd4b44060987
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-04-17 13:05:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 31.3 MB (80.13%)

--- my-solutions/0792-binary-search/solution.cpp
+++ my-solutions/0792-binary-search/solution.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0, r = size(nums) - 1;
+        int l = 0, r = nums.size() - 1;
         while(l <= r) {
-            int mid = (l + r) / 2;
-            if(nums[mid] == target)
-                return mid;
-            else if(nums[mid] > target)
-                r = mid-1;
-            else
-                l = mid + 1;
+            int m = l + (r - l) / 2;
+            if(nums[m] == target) return m;
+            if(nums[m] > target) r = m - 1;
+            else l = m + 1;
         }
+
         return -1;
     }
 };
