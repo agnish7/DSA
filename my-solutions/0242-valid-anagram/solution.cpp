8012c12edc30a0a42bbf08bd4997a739ff3e0d0d
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2025-05-10 07:26:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.7 MB (74.68%)

--- my-solutions/0242-valid-anagram/solution.cpp
+++ my-solutions/0242-valid-anagram/solution.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char, int> char_count;
-        for(int i = 0; i < size(s); i++){
-            if(char_count.find(s[i]) == char_count.end()){
-                char_count.insert(pair<char, int>(s[i], 1));
-            } else {
-                char_count[s[i]]++;
-            }
+        int cache[26] = {0};
+        for(auto& c: s) {
+            ++cache[c - 'a'];
         }
-        for(int i = 0; i < size(t); i++) {
-            if(char_count.find(t[i]) == char_count.end()) {
-                return false;
-            } else {
-                char_count[t[i]]--;
-                if(char_count[t[i]] < 0) {
-                    return false;
-                }
-            }
+        for(auto& c: t) {
+            --cache[c - 'a'];
+            if(cache[c - 'a'] < 0) return false;
         }
-        for(auto &i: char_count) {
-            if(i.second != 0) {
-                return false;
-            }
+
+        for(auto& c: cache) {
+            if(c > 0) return false;
         }
+
         return true;
     }
 };
+
