e99e98f55e6209d95a9b58369c180dc284fb6747
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2026-01-24 23:49:01 +0000

    Sync LeetCode submission Runtime - 9 ms (27.87%), Memory - 22.7 MB (23.44%)

--- my-solutions/0155-min-stack/solution.py
+++ my-solutions/0155-min-stack/solution.py
@@ -1,33 +1,21 @@
 class MinStack:
 
     def __init__(self):
-        self.mainStack = deque()
-        self.minStack = deque()
+        self.stack = deque()
+        self.minstack = deque()
 
     def push(self, val: int) -> None:
-        if len(self.mainStack) == 0:
-            self.minStack.append(val)
-        else:
-            self.minStack.append(min(self.minStack[-1], val))
-        self.mainStack.append(val)
+        self.stack.append(val)
+        self.minstack.append(val if not self.minstack else min(val, self.getMin()))
 
     def pop(self) -> None:
-        if len(self.mainStack) > 0:
-            self.mainStack.pop()
-            self.minStack.pop()
-
+        self.stack.pop()
+        self.minstack.pop()
 
     def top(self) -> int:
-        return self.mainStack[-1]
+        if self.stack:
+            return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.minStack[-1]
-        
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+        if self.minstack:
+            return self.minstack[-1]
