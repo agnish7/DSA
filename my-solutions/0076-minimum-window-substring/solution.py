8d10c6d1a71931e426e678472a2eec5c2c51f1d1
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2026-04-25 21:38:28 +0000

    Sync LeetCode submission Runtime - 42 ms (98.06%), Memory - 19.7 MB (37.35%)

--- my-solutions/0076-minimum-window-substring/solution.py
+++ my-solutions/0076-minimum-window-substring/solution.py
@@ -1,26 +1,37 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        sCount, tCount = defaultdict(int), defaultdict(int)
+        if len(s) < len(t):
+            return ""
+
+        matches = 0
+        scount, tcount = defaultdict(int), defaultdict(int)
+
         for c in t:
-            tCount[c] += 1
-        need = len(list(tCount.keys()))
-        l, have = 0, 0
-        minWindow, minL, minR = float('inf'), -1, -1
+            tcount[c] += 1
+
+        needed = len(tcount)
+        l = 0
+        min_window = float('inf')
+        min_l, min_r =  -1, -1
+
         for r in range(len(s)):
-            sCount[s[r]] += 1
-            if sCount[s[r]] == tCount[s[r]]:
-                have += 1
-            while(have == need):
-                # update the min window and l, r indices
-                if r - l < minWindow:
-                    minWindow = r - l
-                    minR = r
-                    minL = l
-                sCount[s[l]] -= 1
-                if sCount[s[l]] == tCount[s[l]] - 1:
-                    have -= 1
-                l += 1
+            c = s[r]
+            scount[c] += 1
+            if scount[c] == tcount[c]:
+                matches += 1
 
+            if matches == needed:
+                while matches == needed:
+                    c = s[l]
+                    scount[c] -= 1
+                    if scount[c] == tcount[c] - 1:
+                        matches -= 1
+                    l += 1
 
-        return s[minL:minR+1] if minWindow < float('inf') else ""
+                if r - (l - 1) + 1 < min_window:
+                    min_window = r - (l - 1) + 1
+                    min_r = r
+                    min_l = l - 1
             
+        
+        return "" if min_window == float('inf') else s[min_l: min_r + 1]
