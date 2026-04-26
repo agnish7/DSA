f3fb946aea7656416faf58fa0c766278a78ca186
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2026-04-26 02:46:14 +0000

    Sync LeetCode submission Runtime - 231 ms (25.52%), Memory - 41.9 MB (31.06%)

--- my-solutions/0883-car-fleet/solution.py
+++ my-solutions/0883-car-fleet/solution.py
@@ -1,28 +1,19 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        posSpeed = [[position[i], speed[i]] for i in range(len(speed))]
-        posSpeed.sort(key = lambda a: a[0], reverse = True)
-        stack = deque()
-        
-        def clash(ps1, ps2):
-            if ps1[1] == ps2[1]:
-                return ps1[0] == ps2[0]
-            else:
-                t = float(ps2[0] - ps1[0]) / float(ps1[1] - ps2[1])
-                # print(ps1[0], ps1[1], ps2[0], ps2[1])
-                x = float(ps1[0]) + float(ps1[1]) * t
-                # print(t, x)
-                return t >= 0 and x <= target
-        
-        for i in range(len(position)):
-            if i == 0:
-                stack.append(i)
-                continue
-            # print(i, stack)
-            # print(posSpeed[i], posSpeed[stack[-1]])
-            if clash(posSpeed[i], posSpeed[stack[-1]]):
-                continue
-            else:
-                stack.append(i)
-            
-        return len(stack)
+
+        def meets(car1, car2):
+            return (target - car1[0]) / car1[1] <= (target - car2[0]) / car2[1]
+
+        cars = [(position[i], speed[i]) for i in range(len(position))]
+        cars.sort()
+
+        s = deque()
+
+        for car in cars:
+            while s and meets(s[-1], car):
+                s.pop()
+            s.append(car)
+
+
+
+        return len(s)
