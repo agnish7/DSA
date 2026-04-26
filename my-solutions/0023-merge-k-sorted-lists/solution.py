f6a63e7fa6372cf6c95f536b3d0166a6c234aa27
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2026-04-26 03:50:19 +0000

    Sync LeetCode submission Runtime - 244 ms (12.27%), Memory - 23.3 MB (9.08%)

--- my-solutions/0023-merge-k-sorted-lists/solution.py
+++ my-solutions/0023-merge-k-sorted-lists/solution.py
@@ -3,68 +3,51 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
-    def merge(self, list1, list2):
-        head = ListNode(0)
-        curr = head
+    def merge2Lists(self, a, b):
+        dummy = ListNode(0)
+        tail = dummy
 
-        while(list1 and list2):
-            if list1.val <= list2.val:
-                curr.next = list1
-                list1 = list1.next
+        while a and b:
+            if a.val <= b.val:
+                tail.next = a
+                a = a.next
             else:
-                curr.next = list2
-                list2 = list2.next
-            curr = curr.next
+                tail.next = b
+                b = b.next
+            tail = tail.next
 
-        if list1:
-            curr.next = list1
-        if list2:
-            curr.next = list2
+        if a:
+            tail.next = a
 
-        return head.next
+        if b:
+            tail.next = b
+
+        return dummy.next
+
+    # def printList(self, head):
+    #     while(head):
+    #         print(head.val, end = " ")
+    #         head = head.next
+    #     print()
     
-    def printList(self, list):
-        while(list):
-            if list.next == None:
-                print(list.val, end = "")
-            else:
-                print(list.val, "->", end = "")
-            list = list.next
-
-        print("")
-    
-    def printAll(self, lists):
-        for list in lists:
-            self.printList(list)
-        print("")
-
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        def mergeLists(lists):
-            if len(lists) <= 1:
-                return lists
-            size = len(lists)
-            for i in range(size // 2):
-                idx = 2 * i
-                lists[i] = self.merge(lists[idx], lists[idx + 1])
+        n = len(lists)
+        print(lists)
 
-            # self.printAll(lists)
-
-            if size % 2 == 0:
-                lists = lists[:size // 2]
-            else:
-                last = lists[-1]
-                lists = lists[:size // 2]
-                lists.append(last)
-
-            # self.printAll(lists)
-    
-            return mergeLists(lists)
-        
-        res = mergeLists(lists)
-        if res:
-            return res[0]
-        else:
+        if n == 0:
             return None
+        if n == 1:
+            return lists[0]
 
+        left = self.mergeKLists(lists[0:n//2])
+        # print(lists[0:n//2])
+        # self.printList(left)
+
+        right = self.mergeKLists(lists[n//2: n])
+        # print(lists[n//2 + 1: n])
+        # self.printList(right)
+
+        return self.merge2Lists(left, right)
 
