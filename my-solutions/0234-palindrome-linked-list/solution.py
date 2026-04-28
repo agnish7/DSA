3f5ca5fbd796903b144aea913ad3d8be602940d1
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2026-04-28 02:11:42 +0000

    Sync LeetCode submission Runtime - 16 ms (93.66%), Memory - 53.6 MB (15.78%)

---
+++ my-solutions/0234-palindrome-linked-list/solution.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        tmp = []
+        while head:
+            tmp.append(head.val)
+            head = head.next
+
+        for i in range(len(tmp) // 2):
+            if tmp[i] != tmp[len(tmp) - 1 - i]:
+                return False
+
+        return True 
