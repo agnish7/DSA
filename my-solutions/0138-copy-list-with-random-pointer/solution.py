0c540202bf0bfacf5e35d70f901b84450f4a67eb
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2026-05-01 03:20:41 +0000

    Sync LeetCode submission Runtime - 36 ms (64.71%), Memory - 13.1 MB (87.11%)

--- my-solutions/0138-copy-list-with-random-pointer/solution.py
+++ my-solutions/0138-copy-list-with-random-pointer/solution.py
@@ -1,50 +1,27 @@
 """
 # Definition for a Node.
 class Node:
-    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+    def __init__(self, x, next=None, random=None):
         self.val = int(x)
         self.next = next
         self.random = random
 """
 
-class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        # without hashmap
-        if not head:
-            return None
+class Solution(object):
+    def copyRandomList(self, head):
+        nodemap = {}
+        nodemap[None] = None
+        tmp = head
 
-        dummyAdder = head
-        while(dummyAdder.next):
-            temp = dummyAdder.next
-            dummyAdder.next = Node(dummyAdder.val)
-            dummyAdder.next.next = temp
-            dummyAdder = dummyAdder.next.next
+        while tmp:
+            nodemap[tmp] = Node(tmp.val)
+            tmp = tmp.next
 
-        dummyAdder.next = Node(dummyAdder.val)
+        tmp = head
+        while tmp:
 
-        # temp = head
-        # while(temp):
-        #     print(temp, temp.val)
-        #     temp = temp.next
-        # return head
-        
-        randomAdder = head
-        while(randomAdder and randomAdder.next):
-            temp = randomAdder.next.next
-            if randomAdder.random:
-                randomAdder.next.random = randomAdder.random.next
-            randomAdder = temp
+            nodemap[tmp].next = nodemap[tmp.next]
+            nodemap[tmp].random = nodemap[tmp.random]
+            tmp = tmp.next
 
-        splitter = head
-        res = head.next
-        while(splitter.next.next):
-            temp = splitter.next.next
-            splitter1 = splitter
-            splitter2 = splitter.next
-            splitter1.next = splitter1.next.next
-            splitter2.next = splitter2.next.next
-            splitter = temp
-        
-        return res
-
-
+        return nodemap[head]
