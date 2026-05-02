e68e95d4be4715051128fec7918c44389607f617
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2026-05-02 16:59:00 +0000

    Sync LeetCode submission Runtime - 12 ms (28.88%), Memory - 19.7 MB (33.11%)

--- my-solutions/0039-combination-sum/solution.py
+++ my-solutions/0039-combination-sum/solution.py
@@ -1,23 +1,25 @@
-class Solution(object):
-    def combinationSum(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-
-        if target < min(candidates):
-            return []
-
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
         res = []
-        for i in range(len(candidates)):
-            if candidates[i] == target:
-                res.append([candidates[i]])
-            elif candidates[i] < target:
-                temp = self.combinationSum(candidates[i:], target - candidates[i])
-                for j in range(len(temp)):
-                    temp2 = [candidates[i]]
-                    temp2.extend(temp[j])
-                    res.append(temp2)
-        
+        stack = []
+        amount = target
+        n = len(nums)
+
+        def dfs(i):
+            nonlocal amount
+
+            if amount == 0:
+                res.append(stack.copy())
+                return
+            if i == n or amount < 0:
+                return
+
+            stack.append(nums[i])
+            amount -= nums[i]
+            dfs(i)
+            amount += nums[i]
+            stack.pop()
+            dfs(i + 1)
+
+        dfs(0)
         return res
