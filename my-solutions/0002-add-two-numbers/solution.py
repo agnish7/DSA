c3ee352122bff393cdac6ede8e3709fe5383a1d8
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2026-05-03 20:25:44 +0000

    Sync LeetCode submission Runtime - 5 ms (32.73%), Memory - 19.3 MB (78.99%)

--- my-solutions/0002-add-two-numbers/solution.py
+++ my-solutions/0002-add-two-numbers/solution.py
@@ -3,43 +3,42 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        summa, carry = 0, 0
-        res, head = None, None
+        dummy = ListNode(0)
+        tail = dummy
+        s, c = 0, 0
 
-        while(l1 and l2):
-            summa = (l1.val + l2.val + carry) % 10
-            newNode = ListNode(summa)
-            if res:
-                res.next = newNode
-                res = res.next
-            else:
-                res = newNode
-                head = res
-            carry = (l1.val + l2.val + carry) // 10
-            l1, l2 = l1.next, l2.next
-
-        while(l1):
-            summa = (l1.val + carry) % 10
-            newNode = ListNode(summa)
-            res.next = newNode
-            res = res.next
-            carry = (l1.val + carry) // 10
-            l1 = l1.next
+        a, b = l1, l2
+        while a and b:
+            s = a.val + b.val + c % 10
+            d = s % 10
+            tail.next = ListNode(d)
+            c = s // 10
+            a = a.next
+            b = b.next
+            tail = tail.next
         
-        while(l2):
-            summa = (l2.val + carry) % 10
-            newNode = ListNode(summa)
-            res.next = newNode
-            res = res.next
-            carry = (l2.val + carry) // 10
-            l2 = l2.next
+        while a:
+            s = c + a.val
+            d = s % 10
+            tail.next = ListNode(d)
+            c = s // 10
+            a = a.next
+            tail = tail.next
 
-        if carry:
-            res.next = ListNode(carry)
-        
-        res = None
+        while b:
+            s = c + b.val
+            d = s % 10
+            tail.next = ListNode(d)
+            c = s // 10
+            b = b.next
+            tail = tail.next
 
-        return head
-        
+        print(c)
+
+        if c == 1:
+            tail.next = ListNode(c)
+
+        return dummy.next
