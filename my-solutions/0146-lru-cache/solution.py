f6f4e954c43c92069276ee8c0c4e1d77fdd41893
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2026-05-04 01:21:49 +0000

    Sync LeetCode submission Runtime - 114 ms (69.25%), Memory - 76.7 MB (91.73%)

--- my-solutions/0146-lru-cache/solution.py
+++ my-solutions/0146-lru-cache/solution.py
@@ -1,57 +1,24 @@
-class Node:
-    def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.left = self.right = None
-
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.right, self.left = Node(0, 0), Node(0, 0)
-        self.right.next, self.left.prev = None, None
-        self.right.prev, self.left.next = self.left, self.right
+        self.cache = OrderedDict()
         self.cap = capacity
-        self.cache = {}
-    
-    def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
 
-    def insert(self, node): # insert at right
-        prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.prev, node.next = prev, nxt
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
-        else:
-            return -1
-        
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return -1
+
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.cache[key].val = value
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-        else:
-            if len(self.cache) == self.cap:
-                lru = self.left.next
-                self.remove(lru)
-                del self.cache[lru.key]
-
-                
-
-            node = Node(key, value)
-            self.cache[key] = node
-            self.insert(node)
-            
-            # temp = self.left
-            # while(temp):
-            #     print(temp.key, temp.val)
-            #     temp = temp.next
-            # print("")
+            self.cache.move_to_end(key)
         
+        self.cache[key] = value
+
+        if len(self.cache) > self.cap:
+            self.cache.popitem(last = False)
 
 
 # Your LRUCache object will be instantiated and called as such:
