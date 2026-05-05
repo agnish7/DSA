3bc7f51904e7002947de699a759a2b552c25bb9e
Agnish Dutta <30212470+agnish7@users.noreply.github.com>
2026-05-05 18:44:24 +0000

    Sync LeetCode submission Runtime - 10 ms (28.54%), Memory - 20.3 MB (36.84%)

--- my-solutions/0073-set-matrix-zeroes/solution.py
+++ my-solutions/0073-set-matrix-zeroes/solution.py
@@ -1,20 +1,29 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        rows = set([])
-        cols = set([])
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j] == 0:
-                    rows.add(i)
-                    cols.add(j)
-        
-        for row in rows:
-            for j in range(len(matrix[0])):
-                matrix[row][j] = 0
-                
-        for i in range(len(matrix)):
-            for col in cols:
-                matrix[i][col] = 0
+
+        row0 = False
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+
+                    if r > 0:
+                        matrix[r][0] = 0
+                    elif r == 0:
+                        row0 = True
+
+        print(matrix)
+
+        for r in range(1, len(matrix)):
+            for c in range(1, len(matrix[0])):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+    
+        if matrix[0][0] == 0:
+            for r in range(len(matrix)):
+                matrix[r][0] = 0                
+
+        if row0 == True:
+            for c in range(len(matrix[0])):
+                matrix[0][c] =  0
+
